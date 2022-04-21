fe2a4497e93e530c7dae192da2cc1cb7b0233701
Zoma Aryo Widagdo <zozozzzoo2@rocketmail.com>
2022-04-21 09:58:43 +0700

    Next Deploy

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,48 +1,24 @@
 "use strict";
 
 const axios = require("axios");
+const { getCurrentTimestamp, snap } = require("../helpers/Midtrans");
 
 class Controller {
   static getChart(req, res, next) {
     const options = {
       method: "GET",
       url: "https://billboard2.p.rapidapi.com/billboard_global_200",
-      params: { date: "2020-09-19" },
+      params: { date: "2022-04-17" },
       headers: {
         "X-RapidAPI-Host": process.env.HOST_CHART,
-        "X-RapidAPI-Key": process.env.KEY_CHART,
+        "X-RapidAPI-Key": "2202f4129amsh3db77d9573b4d9cp1bea67jsnadca2d22401c",
       },
     };
 
     axios
       .request(options)
       .then(function (response) {
-        const data = response.data.slice(0, 19);
-
-        res.status(200).json({
-          data,
-        });
-      })
-      .catch(function (error) {
-        next(error);
-      });
-  }
-
-  static getTopArtist(req, res, next) {
-    const options = {
-      method: "GET",
-      url: "https://billboard2.p.rapidapi.com/artist_100",
-      params: { date: "2020-03-18" },
-      headers: {
-        "X-RapidAPI-Host": process.env.HOST_CHART,
-        "X-RapidAPI-Key": process.env.KEY_CHART,
-      },
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        const data = response.data.slice(0, 19);
+        const data = response.data.slice(0, 25);
 
         res.status(200).json({
           data,
@@ -67,7 +43,7 @@ class Controller {
     axios
       .request(options)
       .then(function (response) {
-        const data = response.data;
+        const data = response.data.slice(0);
 
         res.status(200).json({
           data,
@@ -81,7 +57,7 @@ class Controller {
   static getLyrics(req, res, next) {
     let { name, title } = req.params;
 
-    title = title.split(" ").join("-");
+    title = title.split(" ").join("-").toLowerCase();
     console.log(title, name);
     const options = {
       method: "GET",
@@ -104,6 +80,39 @@ class Controller {
       .catch(function (error) {
         next(error);
       });
+  }
+
+  static async midTrans(req, res, next) {
+    try {
+      const { email, amount } = req.body;
+      let id = "donate-" + getCurrentTimestamp();
+
+      let parameter = {
+        transaction_details: {
+          order_id: id,
+          gross_amount: amount,
+        },
+        credit_card: {
+          secure: true,
+        },
+        customer_details: {
+          email: email,
+        },
+      };
+
+      let donation = await snap.createTransaction(parameter);
+
+      let token = donation.token;
+
+      let redirect_url = donation.redirect_url;
+
+      res.status(200).json({
+        token,
+        redirect_url,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 }
 module.exports = Controller;
--- routers/router.js
+++ routers/router.js
@@ -6,8 +6,8 @@ const Controller = require("../controllers/controller");
 const router = express.Router();
 
 router.get("/Chart", Controller.getChart);
-router.get("/TopArtist", Controller.getTopArtist);
 router.get("/Song/:name", Controller.searchSong);
 router.get("/Song/:name/:title", Controller.getLyrics);
+router.post("/donation", Controller.midTrans);
 
 module.exports = router;
